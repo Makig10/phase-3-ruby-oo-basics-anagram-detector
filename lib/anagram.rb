1feb8a482af866b54e409da5b6a013d6817bb75f
Brian <mahuibrian14@gmail.com>
2023-05-23 12:28:40 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word.downcase
+  end
+  
+  def match(words)
+    words.select { |w| anagram?(w.downcase) }
+  end
+  
+  private
+  
+  def anagram?(other_word)
+    word.chars.sort == other_word.chars.sort && word != other_word
+  end
+end
+
+
+anagram = Anagram.new('listen')
+puts anagram.match(['enlists', 'google', 'inlets', 'banana']) 
